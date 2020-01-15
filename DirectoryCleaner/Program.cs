489460d3a3906ad8f0faa6d1ce54cc8580b6b66d
James Shelley <james.shelley@greenwoodcampbell.com>
2020-01-15 22:09:31 +0000

    Exploring windows directory classes

--- DirectoryCleaner/Program.cs
+++ DirectoryCleaner/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
 
 namespace DirectoryCleaner
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                // Only get files that begin with the letter "c".
+                string[] dirs = Directory.GetFiles(@"c:\users\james\Desktop", "d*");
+                Console.WriteLine("The number of files starting with c is {0}.", dirs.Length);
+                foreach (string dir in dirs)
+                {
+                    DateTime dt = Directory.GetLastAccessTime(dir);
+                    Console.WriteLine("File: " + dir + ", Last Access Time: " + dt);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            Console.ReadLine();
         }
+
     }
 }
